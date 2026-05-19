2a8aa3ca87234fa47eb7a8777ee32db8ffbecf09
stacie6289 <stacie.takushi@student.uagc.edu>
2026-05-18 19:40:08 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+import StreamList from './components/StreamList';
+import Movies from './components/Movies';
+import Cart from './components/Cart';
+import About from './components/About';
+
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div>
+      <h1>StreamList Home</h1>
+
+      <Router>
+        <NavBar />
+          <Routes>
+            <Route path='/' element={<StreamList />} />
+            <Route path='/' element={<Movies />} />
+            <Route path='/' element={<Cart />} />
+            <Route path='/' element={<About />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
