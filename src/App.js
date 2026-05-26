2290929d0dd7d1148a004a158f9b7b06b06af1e5
stacie6289 <stacie.takushi@student.uagc.edu>
2026-05-26 15:48:06 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
 
+import NavBar from './components/NavBar';
 import StreamList from './components/StreamList';
 import Movies from './components/Movies';
 import Cart from './components/Cart';
@@ -12,17 +12,13 @@ import './App.css';
 function App() {
   return ( 
     <div>
-      <h1>StreamList Home</h1>
-
-      <Router>
         <NavBar />
-          <Routes>
-            <Route path='/' element={<StreamList />} />
-            <Route path='/' element={<Movies />} />
-            <Route path='/' element={<Cart />} />
-            <Route path='/' element={<About />} />
-          </Routes>
-      </Router>
+        <Routes>
+          <Route path="/" element={<StreamList />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
     </div>
   );
 }
