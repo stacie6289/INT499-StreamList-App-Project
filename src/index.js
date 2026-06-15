7975354e42c8e21fe3d4e799c3fac2ed51925243
stacie6289 <stacie.takushi@student.uagc.edu>
2026-06-15 14:37:17 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -12,7 +12,23 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/serviceworker.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker Registered:",
+          registration
+        );
+      })
+      .catch((error) => {
+        console.log(
+          "Service Worker Registration Failed:",
+          error
+        );
+      });
+  });
+}
+
 reportWebVitals();
